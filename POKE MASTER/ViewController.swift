80b2e2900ca24b8a2f2c20306ef24ec1215db386
ประเสริฐ แสงแก้ว <kenglex@hotmail.com>
2018-11-10 01:24:13 +0700

    Initial Commit

---
+++ POKE MASTER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POKE MASTER
+//
+//  Created by kengserch on 10/11/2561 BE.
+//  Copyright © 2561 ICTSUIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
